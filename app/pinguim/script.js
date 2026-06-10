4c177092464192aea57b5441db99e3e0f4990e34
Mathias Souza <mathias.rodrigues77@gmail.com>
2026-06-10 08:34:00 -0300

    1006260833

--- app/pinguim/script.js
+++ app/pinguim/script.js
@@ -18,19 +18,7 @@
     function onConnectSuccess(broker) {
       console.log("Mqtt conectado ao broker: ", broker);
     }
-    //--------Callback MQTT------
-    function processarDados(topic, data, broker = null) {
-      console.log("Broker: ", broker);
-      console.log("Topic: ", topic);
-      console.log("payload: ", data);
-      if (broker == "HiveMQ" && topic.split("/")[1] == "device_1") {
-        //mqtt.hivemq.obj.publish(topic.split("/")[0] + "/teste", "ok");
-        //mqtt.crematec.obj.unSubscribeAll();
-      }
-    }
-
-
-
+  
     //Conectando ao broker HiveMQ
     mqtt.hivemq.obj = new MqttService(
       mqtt.hivemq.host,
